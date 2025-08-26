a31caa5dd569ea12776c853ad075deeb68979f72
Lucille Eva Gorbe <gorbele@umich.edu>
2025-08-26 17:27:07 -0400

    Add files via upload
    hello world upload as detailed by Dan

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+# Welcome back to beautiful, beautiful python, a coding language that, 
+#       despite being named after a constricting snake, is anything but restrictive.
+
+# This module is a demonstration of coding language, environment, and operator readiness.
+# Print 'Hello World.' to demonstrate functionality.
+print('Hello World.')
