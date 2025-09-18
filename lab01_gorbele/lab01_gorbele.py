3db9a636b5282edc921bbb1ad8463c48357a6aac
Lucille Gorbe <gorbele@umich.edu>
2025-09-18 11:20:25 -0400

    Function 1 complete and testing

--- lab01_gorbele/lab01_gorbele.py
+++ lab01_gorbele/lab01_gorbele.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pyton3
+#!/usr/bin/env python3
 '''
 This file solves the N-Layer atmosphere problem for Lab 01 and all subparts.
 
@@ -15,9 +15,26 @@ sigma = 5.67E-8 #W/m2/K-4
 
 #function that accepts nlayers, S0, epsilon, and albedo {
 
-def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350):
+def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350, debug=False):
     '''
-    
+    This function simulates an n-layer atmosphere.
+    -----
+    Parameters
+    -----
+    nlayers - int
+        The number of atmospheric layers included in this atmosphere model.
+    epsilon - float
+        Defaults to 1, the emissivity of all layers in this model.
+    albedo - float
+        Defaults to 0.33, the average reflectivity of the body's surface.
+    s0 - int
+        Defaults to 1350, the solar forcing constant (W/m2) at the body's top-of-atmosphere.
+    debug - bool
+        Defaults to false, enters debug mode.
+    -----
+    Returns
+    -----
+
     '''
 
     # Create array of coefficients, an N+1xN+1 array:
@@ -25,14 +42,27 @@ def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350):
     b = np.zeros(nlayers+1)
 
     # Populate based on model
-    for i in range(nlayers+1):
+    for i in range(nlayers+1): 
         for j in range(nlayers+1):
-            #A[i,j] = 
+            if (i == j):
+                A[i,j] = - 2 + 1 * (i==0) #maybe faster than if statements
+            else: 
+                A[i,j] = (1-epsilon)**(np.abs(i-j)-1) * epsilon**(i>0)
+            # row major: i is tbe row, j is the column, so loops through first row
+            #write it so its expressed entirely as I, J, and epsilon
+    if debug:
+        print(A)
+    b[0] = -0.25 * s0 * (1-albedo)
 
-    #b = 
+    #Invert matrix
+    Ainv = inv(A)
+    # get solutions
+    fluxes = np.matmul(Ainv, b)
 
-#}
+    temp = (fluxes/sigma)**(1/4)
+    return temp
 
+temps_4 = n_layer_atmos(4)
 #plot temp function
 
 #compare test cases function
@@ -61,6 +91,3 @@ def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350):
             #earth not in energy balance, has shortcomings
             #misses other forms of energy transport
             #winds, weathers, latent heat, terrestrial heat
-
-
-    
