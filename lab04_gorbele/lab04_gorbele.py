f3b76580a63c0d0b5d1f2fe91d94b230799a60bc
Lucille Gorbe <gorbele@umich.edu>
2025-11-06 11:17:55 -0500

    Add tracking and plotting of forest percentages; start function for plotting the forest over time; begin logical indexing work

--- lab04_gorbele/lab04_gorbele.py
+++ lab04_gorbele/lab04_gorbele.py
@@ -25,10 +25,11 @@ be able to run any additional functions.
 
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import random
+from numpy.random import rand
 plt.style.use("seaborn-v0_8")
 
-def spread(nstep=4, isize=3, jsize=3, pspread=1.0):
+
+def spread(nstep=4, isize=3, jsize=3, pspread=1.0, pignite=0.1, pbare=0):
     '''
     Me when my fire is spreading
 
@@ -39,55 +40,127 @@ def spread(nstep=4, isize=3, jsize=3, pspread=1.0):
     isize, jsize : ints, defaults to 3 and 3, respectively
         The spatial dimensions of the population under consideration.
     pspread : float, defaults to 1.0
-        Sets chance that fire will spread
+        Sets chance that fire will spread.
+    pignite : float, defaults to 0.1
+        Sets chance that each cell will be a source of forest fire ignition.
     '''
 
-    #Create our little guys and make them all normalsauce
+    # Create our little guys and make them all normalsauce
     area = np.zeros((nstep, isize, jsize)) + 2
 
-    #if nothing is on fire, stop! return only the on-fire steps to reduce load
+    # if nothing is on fire, stop! return only the on-fire steps to reduce load
 
-    # Set initial fire on center [UPDATE FOR LAB LATER]
-    area[0, isize//2, jsize//2] = 3
+    # Set initial conditions for burning/infected and bare/immune
+    # Set bare/immune first:
+    bare = pbare > rand(isize, jsize)
+    area[0, bare] = 1
 
+    # Now for burning/infected, include singular ignition point test case
+    # Via if statement; 0 chance of ignition indicates single point ignition
+    if pignite == 0.:
+        # Set initial fire to center:
+        area[0, isize//2, jsize//2] = 3
+    else:  # Scatter fire randomly:
+        ignite = np.zeros((isize, jsize), dtype=bool)
+        # Set minimum and maximum number of ignition sites, loop until in range
+        while (ignite.sum() == 0) | (ignite.sum() > 0.10 * isize * jsize):
+            ignite = pignite > rand(isize, jsize)
+        print(f"Starting with {ignite.sum()} points on fire or infected.")
+        area[0, ignite] = 3
+
+    perconv = 100. / (isize * jsize)
+
+    perfire = np.zeros(nstep)
+    perfire[0] = (area[0, :, :] == 3).sum * perconv
+    perforest = np.zeros(nstep)
+    perforest[0] = (area[0, :, :] == 2).sum * perconv
+    perbare = np.zeros(nstep)
+    perbare[0] = (area[0, :, :] == 1).sum * perconv
+    perdead = np.zeros(nstep)
+
+    # run thru time....
     for t in range(nstep-1):
+        if (area[t, :, :] == 3).sum() == 0:
+            print(f"No more tiles on fire; simulation terminated early at t={t}")
+            break
+        # Initialize next time step as current time step
         area[t+1, :, :] = area[t, :, :]
-        for i in range(isize):
-            for j in range(jsize):
+        can_spread = pspread > rand(isize, jsize)
+        # grab locations of fires
+        fires = np.transpose((area[t, :, :] == 3).nonzero())
+        
+        #check the boundaries of the fires and execute commands off of those
+        for f in fires:
+
+
+        #for i in range(isize):
+            #for j in range(jsize):
+
                 # is there fire????
-                if area[t, i, j] != 3:
+                #if area[t, i, j] != 3:
                     # Skip past this lil guy
-                    continue
-                #pain and fire spreading time
-                #criteria to check: only spread to forest
+                    #continue
+
+                # pain and fire spreading time
+                # criteria to check: only spread to forest
                 # do not exceed bounds
                 # Spread north
-                if (pspread > random.rand()) & (i > 0    ) & (area[t, i-1, j] == 2):
-                    area[t+1, i-1, j] = 3
-                # Spread south
-                if (pspread > random.rand()) & (i < isize) & (area[t, i+1, j] == 2):
-                    area[t+1, i+1, j] = 3
-                # Spread west
-                if (pspread > random.rand()) & (j > 0    ) & (area[t, i, j-1] == 2):
-                    area[t+1, i, j-1] = 3
-                # Spread east
-                if (pspread > random.rand()) & (j < jsize) & (area[t, i, j+1] == 2):
-                    area[t+1, i, j+1] = 3
+                #if (pspread > rand()) & (i > 0    ) & (area[t, i-1, j] == 2):
+                #    area[t+1, i-1, j] = 3
+                ## Spread south
+                #if (pspread > rand()) & (i < isize) & (area[t, i+1, j] == 2):
+                #    area[t+1, i+1, j] = 3
+                ## Spread west
+                #if (pspread > rand()) & (j > 0    ) & (area[t, i, j-1] == 2):
+                #    area[t+1, i, j-1] = 3
+                ## Spread east
+                #if (pspread > rand()) & (j < jsize) & (area[t, i, j+1] == 2):
+                #    area[t+1, i, j+1] = 3
+
+                #make this better w/ logical indexing, make a random thingymabob
+                #pchance > rand & awesome > true !!
 
                 # make that bare!
-                area[t+1, i, j] = 1
+                #area[t+1, i, j] = 1
+        # report percentage of forest on fire, barren, and forested
+        
 
 
+    #tasks:
+    #initialize fire randomly or by cell (DONE)
+    # set bare (immune) randomly (DONE)
+    # track percent of forest burning, bare, and forested (DONE)
+    # make cool plot
 
 
-def spreadcheck(section, iloc, jloc, pspread):
+def plot_progression(forest):
     '''
-    Check if we are spreading babyyyyy
+    report and plot the percentages of things across time
     '''
 
-    # check if guy is forested in this time step
-    # check roll for spread (random.rand() function)
-        # if ye change to fire
-    # check the guy
+    ksize, isize, jsize = forest.shape()
 
-    return section
+    perconv = 100. / (isize * jsize)
+
+    loc = forest == 2
+    forested = loc.sum(axis=(1, 2)) * perconv
+
+    loc = forest == 3
+    fire = loc.sum(axis=(1, 2)) * perconv
+
+    loc = forest == 1
+    bare = loc.sum(axis=(1, 2)) * perconv
+
+    plt.plot(forested, label='Forested')
+    plt.plot(fire, label='On Fire')
+    plt.plot(bare, label='Bare')
+    plt.xlabel('Time ($UNITS$)')  # fill units later
+    plt.ylabel('Percent')
+
+
+def plot_forest2d():
+    '''
+    Plot the awesome file and stuff
+    '''
+
+    pass
